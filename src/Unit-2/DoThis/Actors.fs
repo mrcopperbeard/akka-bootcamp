ec7ab69334e0b6b0b1db61955348af9bc66bb8e0
mrcopperbeard <mrcoperbeard@mail.ru>
2021-05-30 21:38:48 +0300

    Unit 2 Lessons 1-2

--- src/Unit-2/DoThis/Actors.fs
+++ src/Unit-2/DoThis/Actors.fs
@@ -7,16 +7,32 @@ open Akka.FSharp
 
 [<AutoOpen>]
 module Messages =
-    type InitializeChart =
+    type ChartMessage =
         | InitializeChart of initialSeries: Map<string, Series>
+        | AddSeries of Series
 
 /// Actors used to intialize chart data
 [<AutoOpen>]
 module Actors =
-    let chartingActor (chart: Chart) message =
-        match message with
-        | InitializeChart series ->
-            chart.Series.Clear ()
-            series |> Map.iter (fun k v ->
-                v.Name <- k
-                chart.Series.Add(v))
+    let chartingActor (chart: Chart) (mailbox: Actor<_>) =
+        let rec charting (mapping: Map<string, Series>) = actor {
+            let! message = mailbox.Receive()
+            match message with
+            | InitializeChart series ->
+                chart.Series.Clear ()
+                series |> Map.iter (fun k v ->
+                    v.Name <- k
+                    chart.Series.Add(v))
+                return! charting series
+            | AddSeries series when
+                not <| System.String.IsNullOrEmpty series.Name &&
+                not <| mapping.ContainsKey series.Name ->
+                    let newMapping = mapping.Add (series.Name, series)
+                    chart.Series.Add series
+                    return! charting newMapping
+        }
+
+        charting Map.empty<string, Series>
+
+
+
--- src/Unit-2/DoThis/Form.fs
+++ src/Unit-2/DoThis/Form.fs
@@ -14,17 +14,26 @@ module Form =
     let chartArea1 = new ChartArea(Name = "ChartArea1")
     let legend1 = new Legend(Name = "Legend1")
     let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
+    let btnAddSeries = new Button(Name = "btnAddSeries", Text = "Add Series", Location = Point(540, 300), Size = Size(100, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+
     sysChart.BeginInit ()
     form.SuspendLayout ()
     sysChart.ChartAreas.Add chartArea1
     sysChart.Legends.Add legend1
     sysChart.Series.Add series1
+    form.Controls.Add btnAddSeries
     form.Controls.Add sysChart
     sysChart.EndInit ()
     form.ResumeLayout false
 
     let load (myActorSystem:ActorSystem) =
-        let chartActor = spawn myActorSystem "charting" (actorOf (Actors.chartingActor sysChart))
+        let chartActor = chartingActor sysChart |> spawn myActorSystem "charting"
         let series = ChartDataHelper.randomSeries "FakeSeries1" None None
         chartActor <! InitializeChart(Map.ofList [(series.Name, series)])
+
+        btnAddSeries.Click.Add(fun _ ->
+            let newSeriesName = sysChart.Series.Count + 1 |> sprintf "FakeSeries %i"
+            let newSeries = ChartDataHelper.randomSeries newSeriesName None None
+            chartActor <! AddSeries newSeries)
+
         form
