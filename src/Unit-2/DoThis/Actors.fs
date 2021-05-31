eb9c50ca883fdd347d3cddee413682321954578b
mrcopperbeard <mrcoperbeard@mail.ru>
2021-05-31 23:11:54 +0300

    Unit 2 Lesson 3 Steps 1-4

--- src/Unit-2/DoThis/Actors.fs
+++ src/Unit-2/DoThis/Actors.fs
@@ -1,7 +1,11 @@
 ﻿namespace ChartApp
 
+open System
 open System.Collections.Generic
+open System.Diagnostics
+open System.Drawing
 open System.Windows.Forms.DataVisualization.Charting
+
 open Akka.Actor
 open Akka.FSharp
 
@@ -10,10 +14,105 @@ module Messages =
     type ChartMessage =
         | InitializeChart of initialSeries: Map<string, Series>
         | AddSeries of Series
+        | RemoveSeries of string
+        | Metric of series: string * counterValue: float
+
+    type CounterType =
+        | Cpu
+        | Memory
+        | Disc
+
+    type CounterMessage =
+        | Subscribe of IActorRef
+        | Unsubscribe of IActorRef
+        | GatherMetric
+
+    type CoordinatorMessage =
+        | Watch of CounterType
+        | Unwatch of CounterType
+
+    type ButtomnMessage = Toggle
 
 /// Actors used to intialize chart data
 [<AutoOpen>]
 module Actors =
+    let performanceCounterActor
+        (seriesName: string)
+        (performanceCounterGenerator: unit -> PerformanceCounter)
+        (mailbox: Actor<_>) =
+
+        let counter = performanceCounterGenerator()
+        let gather =
+            mailbox.Context.System.Scheduler.ScheduleTellRepeatedlyCancelable (
+                TimeSpan.FromMilliseconds 250.,
+                TimeSpan.FromMilliseconds 250.,
+                mailbox.Self,
+                GatherMetric,
+                ActorRefs.NoSender)
+
+        mailbox.Defer(fun () ->
+            gather.Cancel()
+            counter.Dispose())
+
+        let rec loop subscriptions = actor {
+            let! message = mailbox.Receive()
+            match box message :?> CounterMessage with
+            | Subscribe(subscriber) ->
+                return! subscriptions
+                |> List.filter (fun s -> s <> subscriber)
+                |> fun tail -> subscriber :: tail
+                |> loop
+            | Unsubscribe(subscriber) ->
+                return! subscriptions
+                |> List.filter (fun s -> s <> subscriber)
+                |> loop
+            | GatherMetric ->
+                let msg = Metric(seriesName, counter.NextValue() |> float)
+                subscriptions |> List.iter (fun s -> s <! msg)
+                return! loop subscriptions
+        }
+
+        loop []
+
+    let performanceCounterCoordinatorActor chartingActor (mailbox: Actor<_>) =
+        let counterGenerators = Map.ofList [
+            Cpu, fun () -> new PerformanceCounter("Processor", "% Processor Time", "_Total", true)
+            Memory, fun () -> new PerformanceCounter("Memory", "% Committed Bytes In Use", true)
+            Disc, fun () -> new PerformanceCounter("LogicalDisk", "% Disk Time", "_Total", true)
+        ]
+
+        let counterSeries = Map.ofList [
+            Cpu, fun _ -> new Series(string Cpu, ChartType = SeriesChartType.SplineArea, Color = Color.DarkGreen)
+            Memory, fun _ -> new Series(string Memory, SeriesChartType.FastLine, Color = Color.MediumBlue)
+            Disc, fun _ -> new Series(string Disc, ChartType = SeriesChartType.SplineArea, Color = Color.DarkRed)
+        ]
+
+        let rec loop (counterActors: Map<CounterType, IActorRef>) = actor {
+            let! message = mailbox.Receive()
+            match message with
+            | Watch counter when counterActors.ContainsKey(counter) |> not ->
+                let counterName = string counter
+                let counterActor =
+                    performanceCounterActor counterName counterGenerators.[counter]
+                    |> spawn mailbox.Context $"counter-{counterName}"
+                let newCounterActors = counterActors.Add(counter, counterActor)
+                chartingActor <! (AddSeries <| counterSeries.[counter]())
+                counterActor <! Subscribe(chartingActor)
+                return! loop newCounterActors
+            | Watch counter ->
+                chartingActor <! (AddSeries <| counterSeries.[counter]())
+                counterActors.[counter] <! Subscribe(chartingActor)
+            | Unwatch counter ->
+                counterSeries.[counter]()
+                |> fun s -> s.Name
+                |> RemoveSeries
+                |> fun m -> chartingActor <! m
+                counterActors.[counter] <! Unsubscribe(chartingActor)
+            return! loop counterActors
+        }
+
+        loop Map.empty<CounterType, IActorRef>
+
     let chartingActor (chart: Chart) (mailbox: Actor<_>) =
         let rec charting (mapping: Map<string, Series>) = actor {
             let! message = mailbox.Receive()
@@ -25,7 +124,7 @@ module Actors =
                     chart.Series.Add(v))
                 return! charting series
             | AddSeries series when
-                not <| System.String.IsNullOrEmpty series.Name &&
+                not <| String.IsNullOrEmpty series.Name &&
                 not <| mapping.ContainsKey series.Name ->
                     let newMapping = mapping.Add (series.Name, series)
                     chart.Series.Add series
--- src/Unit-2/DoThis/Form.fs
+++ src/Unit-2/DoThis/Form.fs
@@ -14,15 +14,20 @@ module Form =
     let chartArea1 = new ChartArea(Name = "ChartArea1")
     let legend1 = new Legend(Name = "Legend1")
     let series1 = new Series(Name = "Series1", ChartArea = "ChartArea1", Legend = "Legend1")
-    let btnAddSeries = new Button(Name = "btnAddSeries", Text = "Add Series", Location = Point(540, 300), Size = Size(100, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+
+    let btnCpu = new Button(Name = "btnCpu", Text = "CPU (ON)", Location = Point(560, 275), Size = Size(110, 40), TabIndex = 1, UseVisualStyleBackColor = true)
+    let btnMemory = new Button(Name = "btnMemory", Text = "MEMORY (OFF)", Location = Point(560, 320), Size = Size(110, 40), TabIndex = 2, UseVisualStyleBackColor = true)
+    let btnDisk = new Button(Name = "btnDisk", Text = "DISK (OFF)", Location = Point(560, 365), Size = Size(110, 40), TabIndex = 3, UseVisualStyleBackColor = true)
 
     sysChart.BeginInit ()
     form.SuspendLayout ()
     sysChart.ChartAreas.Add chartArea1
     sysChart.Legends.Add legend1
     sysChart.Series.Add series1
-    form.Controls.Add btnAddSeries
     form.Controls.Add sysChart
+    form.Controls.Add btnCpu
+    form.Controls.Add btnMemory
+    form.Controls.Add btnDisk
     sysChart.EndInit ()
     form.ResumeLayout false
 
@@ -30,10 +35,7 @@ module Form =
         let chartActor = chartingActor sysChart |> spawn myActorSystem "charting"
         let series = ChartDataHelper.randomSeries "FakeSeries1" None None
         chartActor <! InitializeChart(Map.ofList [(series.Name, series)])
-
-        btnAddSeries.Click.Add(fun _ ->
-            let newSeriesName = sysChart.Series.Count + 1 |> sprintf "FakeSeries %i"
-            let newSeries = ChartDataHelper.randomSeries newSeriesName None None
-            chartActor <! AddSeries newSeries)
-
+        btnCpu.Click.Add (fun _ -> () )
+        btnMemory.Click.Add (fun _ -> () )
+        btnDisk.Click.Add (fun _ -> () )
         form
